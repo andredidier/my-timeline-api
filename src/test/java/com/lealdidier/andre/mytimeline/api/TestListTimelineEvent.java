cbca5596499ceeff6b797a4f5bcf7e6152f6e7d4
Andre Didier <andredidier@users.noreply.github.com>
2018-07-02 00:07:23 -0300

    Configured project and junit5 tests.

---
+++ src/test/java/com/lealdidier/andre/mytimeline/api/TestListTimelineEvent.java
@@ -0,0 +1,21 @@
+package com.lealdidier.andre.mytimeline.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test list of timeline events")
+public class TestListTimelineEvent {
+
+    @Test
+    @DisplayName("No events")
+    public void listNoEvents() {
+
+    }
+
+    @Test
+    @DisplayName("One event")
+    public void listOneEvent() {
+
+    }
+}
